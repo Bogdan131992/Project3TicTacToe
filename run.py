b66a64f3a40bbbb158241c2b6f09999a4a2e912e
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-05-17 18:50:12 +0000

    Create TicTAcToe class

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
+class TicTacToeGame:
+    """
+    Initializesa new instance of the TicTacToe class.
+    The board is represented as 3x3 grid, initially filed with empty spaces.
+    The current player is set to "X" at the game start.
+    """
+    self.board = [["" for _ in range(3)] for _ in range (3)]
+    self.current_player = "X"
+
+
+
 def displayWelcomeMessage ():
+    """
+    Displays a welcome message at the start of the game
+    """
     print ("Welcome to the interactive Tic Tac Toe game")
+
+
 
 
 
