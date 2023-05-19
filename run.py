ef0f3c6c94c2f2efb7461c704f15135afa6a696a
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-05-19 10:41:27 +0000

    Corecting the lenght of lines with more the 79 characters

--- run.py
+++ run.py
@@ -6,16 +6,16 @@ class TicTacToeGame:
         initially filled with empty spaces.
         The current player is set to "X" to start the game.
         """
-        self.board = [[" " for _ in range(3)] for _ in range(3)]
+        self.brd = [[" " for _ in range(3)] for _ in range(3)]
         self.current_player = "X"
 
-    def print_board(self):
+    def print_brd(self):
         """
         Prints the current state of the game board.
         """
         print("   0   1   2")
         for row in range(3):
-            print(f"{row}  " + " | ".join(self.board[row]))
+            print(f"{row}  " + " | ".join(self.brd[row]))
             if row < 2:
                 print("  ---+---+---")
 
@@ -26,23 +26,30 @@ class TicTacToeGame:
         "Tie" for a tie, or None if there is no winner yet.
         """
         # Check rows
-        for row in self.board:
+        for row in self.brd:
             if row[0] == row[1] == row[2] != " ":
                 return row[0]
 
         # Check columns
         for col in range(3):
-            if self.board[0][col] == self.board[1][col] == self.board[2][col] != " ":
+            if self.brd[0][col] == self.brd[1][col] == self.brd[2][col] != " ":
                 return self.board[0][col]
 
         # Check diagonals
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
-            return self.board[0][0]
-        if self.board[0][2] == self.board[1][1] == self.board[2][0] != " ":
-            return self.board[0][2]
+        if self.brd[0][0] == self.brd[1][1] == self.brd[2][2] != " ":
+            return self.brd[0][0]
+        if self.brd[0][2] == self.brd[1][1] == self.brd[2][0] != " ":
+            return self.brd[0][2]
 
         # Check for a tie
-        if all(self.board[row][col] != " " for row in range(3) for col in range(3)):
+        for row in range(3):
+            for col in range(3):
+                if self.brd[row][col] == " ":
+                    break
+            else:
+                continue
+            break
+        else:
             return "Tie"
 
         # No winner yet
@@ -58,27 +65,28 @@ def main():
     The game continues until there is a winner or a tie.
     """
     print("Welcome to the interactive Tic Tac Toe game")
+    print("Your move shoul contain two numbers (row [space] column)")
     game = TicTacToeGame()
 
     while True:
-        game.print_board()
+        game.print_brd()
         try:
-            move = input(f"Player {game.current_player}, enter your move (row [space] column): ")
+            move = input(f"Player {game.current_player}, enter your move: ")
             row, col = map(int, move.split())
 
             if not (0 <= row <= 2 and 0 <= col <= 2):
-                print("Invalid move. The row and column should be between 0 and 2.")
+                print("Invalid move. Enter numbers between 0 and 2.")
                 continue
 
-            if game.board[row][col] != " ":
+            if game.brd[row][col] != " ":
                 print("Invalid move. That position is already occupied.")
                 continue
 
-            game.board[row][col] = game.current_player
+            game.brd[row][col] = game.current_player
 
             winner = game.check_winner()
             if winner:
-                game.print_board()
+                game.print_brd()
                 if winner == "Tie":
                     print("It's a tie!")
                 else:
@@ -88,7 +96,7 @@ def main():
             game.current_player = "O" if game.current_player == "X" else "X"
 
         except ValueError:
-            print("Invalid input. Please enter two integers separated by a space.")
+            print("Invalid input.Enter two integers separated by a space.")
         except KeyboardInterrupt:
             print("\nGame interrupted. Exiting...")
             break
